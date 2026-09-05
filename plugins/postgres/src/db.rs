8a88c78517274c054f4e25bad8cc2044cacf88d6
ferd tomale <beowulf1416@gmail.com>
2026-09-05 22:29:23 +0800

    modify postgres connection manager to ask for password

--- plugins/postgres/src/db.rs
+++ plugins/postgres/src/db.rs
@@ -5,7 +5,25 @@ use tracing::{debug, error};
 use tokio::runtime::{Builder, Runtime};
 use tokio::sync::{OnceCell, RwLock};
 
+#[derive(Debug)]
+pub(super) struct ConnectionSettings {
+    pub(super) name: String,
+    pub(super) db: String,
+    pub(super) host: String,
+    pub(super) port: u16,
+    pub(super) user: String,
+    pub(super) pw: Option<String>,
+}
+
+impl ConnectionSettings {
+    pub fn set_pw(&mut self, pw: &String) {
+        self.pw = pw.clone();
+    }
+}
+
+#[derive(Debug)]
 pub struct ConnectionManager {
+    settings: RwLock<std::collections::HashMap<String, ConnectionSettings>>,
     pools: RwLock<std::collections::HashMap<String, sqlx::Pool<sqlx::Postgres>>>,
 }
 
@@ -14,6 +32,7 @@ static CM: OnceCell<ConnectionManager> = OnceCell::const_new();
 pub async fn get_connection_manager() -> &'static ConnectionManager {
     CM.get_or_init(|| async {
         ConnectionManager {
+            settings: RwLock::new(std::collections::HashMap::new()),
             pools: RwLock::new(std::collections::HashMap::new()),
         }
     })
@@ -28,29 +47,79 @@ impl ConnectionManager {
         host: &String,
         port: u16,
         user: &String,
-        pw: &String,
+        pw: Option<String>,
     ) -> anyhow::Result<sqlx::Pool<sqlx::Postgres>> {
         let uri = format!("postgres://{user}:{pw}@{host}:{port}/{db}");
 
-        match sqlx::postgres::PgPoolOptions::new()
-            .max_connections(5)
-            .connect(&uri)
-            .await
-        {
-            Err(e) => {
-                error!("unable to add connection: {}", e);
-                return Err(anyhow::anyhow!("unable to add connection: {}", e));
+        let settings = self.settings.read().await;
+        match settings.get(name) {
+            Some(s) => {
+                error!("connection already exists {}", name);
+                return Err(anyhow::anyhow!("connection already exists {}", name));
             }
-            Ok(pool) => {
-                let mut pools = self.pools.write().await;
-                pools.insert(name.to_string(), pool.clone());
-                return Ok(pool.clone());
+            None => {
+                let mut settings = self.settings.write().await;
+                settings.insert(
+                    name.clone(),
+                    ConnectionSettings {
+                        name: name.clone(),
+                        db: db.clone(),
+                        host: host.clone(),
+                        port,
+                        user: user.clone(),
+                        pw: pw.clone(),
+                    },
+                );
+
+                return Ok(());
             }
         }
+
+        // match sqlx::postgres::PgPoolOptions::new()
+        //     .max_connections(5)
+        //     // .connect(&uri)
+        //     .await
+        // {
+        //     Err(e) => {
+        //         error!("unable to add connection: {}", e);
+        //         return Err(anyhow::anyhow!("unable to add connection: {}", e));
+        //     }
+        //     Ok(pool) => {
+        //         let mut pools = self.pools.write().await;
+        //         pools.insert(name.to_string(), pool.clone());
+        //         return Ok(pool.clone());
+        //     }
+        // }
     }
 
-    pub async fn get_pool(&self, name: &str) -> Option<sqlx::Pool<sqlx::Postgres>> {
-        let pools = self.pools.read().await;
-        pools.get(name).cloned()
+    pub async fn get_pool(&self, name: &str) -> anyhow::Result<sqlx::Pool<sqlx::Postgres>> {
+        let settings = self.settings.read().await;
+        match settings.get(name.to_string()) {
+            None => {
+                error!("connection does not exist {}", name);
+                return Err(anyhow::anyhow!("connection does not exist {}", name));
+            }
+            Some(s) => {
+                if s.pw.is_none() {
+                    // pw is not set, ask from user
+                } else {
+                    let pools = self.pools.read().await;
+                    match pools.get(name) {
+                        None => {
+                            error!("pool does not exist {}", name);
+                            return Err(anyhow::anyhow!("pool does not exist {}", name));
+                        }
+                        Some(p) => {
+                            return Ok(p.clone());
+                        }
+                    }
+                }
+
+                return Err(anyhow::anyhow!("//todo"));
+            }
+        }
+
+        // let pools = self.pools.read().await;
+        // pools.get(name).cloned()
     }
 }
